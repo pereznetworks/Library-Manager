195acfe992a40d0f47e6afd221c7021394846504
Daniel Perez <pereznetworks@live.com>
2018-10-01 23:14:10 -0700

    OMG, finally for 3 re-usable pugs to work
    createNew, filterTable and table.pug
    
    will re-use for Patrons and Loans

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+/* importing sequelize models */
 var books = require("../models").books;
 var patrons = require("../models").patrons;
 var loans = require("../models").loans;
 
+/* importing locals for rendering in pub templates */
+var locals = require("../views/locals")
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', locals.homePg);
 });
 
+/* GET books page. */
+router.get('/books', function(req, res, next) {
+  res.render('books', locals.booksPg);
+});
 module.exports = router;
