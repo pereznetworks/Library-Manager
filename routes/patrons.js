da2ea857b5fde9169cc8dc6d1fb31ef8635fb098
Daniel Perez <pereznetworks@live.com>
2018-10-10 13:20:27 -0700

    cleaning up old dirs.. adding include to findOne Patron query

--- routes/patrons.js
+++ routes/patrons.js
@@ -2,10 +2,12 @@
 var express = require('express');
 var router = express.Router();
 var createError = require('http-errors');
+var Sequelize = require('../models').sequelize;
 
 /* importing sequelize models */
 var Patrons = require("../models").Patrons;
 var Loans = require("../models").Loans;
+var Books = require("../models").Books;
 
 /* importing locals for rendering in pub templates */
 var locals = require("../views/locals")
@@ -46,35 +48,44 @@ router.get('/patrons/new', function(req, res, next) {
 
 /* GET new patrons form page */
 router.get('/patrons/patron_detail/:id', function(req, res, next) {
-  Loans.findOne({ where: { patron_id:req.params.id } }).then(function(loan){
+
+  res.locals.title = 'Patron: ';
+  res.locals.patronsColumnArray = locals.patronsPg.columnArray;
+  res.locals.loansColumnArray = locals.loansPg.columnArray;
+  res.locals.bookHrefPath = locals.loansPg.bookHrefPath;
+  res.locals.patronHrefPath = locals.loansPg.patronHrefPath;
+  res.locals.actionHrefPath = locals.loansPg.actionHrefPath;
+
+  Patrons.findOne({
+      where: { id:req.params.id },
+      include: [{
+        model: Loans,
+        where: {PatronId: Sequelize.col("patrons.id")},
+        include: [{
+          model: Books,
+          where: {id: Sequelize.col("Loans.BookId")}
+          }]
+      }]
+  }).then(function(patron){
     // TODO: this synatax and object model works
     // but data must be inserted into db tables (sequelize db:seed)
     // for any data to be displayed
-    res.locals.title = 'Patron: ';
-    res.locals.columnArray = locals.loansPg.columnArray;
-    res.locals.bookHrefPath = locals.loansPg.bookHrefPath;
-    res.locals.patronHrefPath = locals.loansPg.patronHrefPath;
-    res.locals.actionHrefPath = locals.loansPg.actionHrefPath;
-    if(!loan) {
-      res.locals.error = createError(200);
-      res.status(200);
-      res.render("patronViews/patron_detail", {rowArray: 'This patron has no loans'});
-    } else {
-      res.render("patronViews/patron_detail", {rowArray: loan.dataValues, patronName: `patron_${req.params.id}`});
-    }
+    res.locals.patronName = `${patron.dataValues.first_name} ${patron.dataValues.first_name}`;
+    res.locals.patronRowArray = patron.dataValues;
+    res.render("patronViews/patron_detail", {rowArray: patron.dataValues, });
+
   }).catch(function(error){
     // set locals, only providing error in development
-    if (error){
-     res.locals.message = error.message;
-     res.locals.error =  error
-   } else {
-     res.locals.message = "Oops, there's been a server error";
-     res.locals.error = createError(500);
-   }
-
-    // render the error page
-    res.status(error.status || 500);
-    res.render('error');
+      if (error){
+        res.locals.message = error.message;
+        res.locals.error =  error
+      } else {
+        res.locals.message = "Oops, there's been a server error";
+        res.locals.error = createError(500);
+      }
+      // render the error page
+      res.status(error.status || 500);
+      res.render('error');
    });
 
 });
