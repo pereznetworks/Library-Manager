f835f7b577717fe719a92180d216117f23e4aaef
Daniel Perez <pereznetworks@live.com>
2018-10-29 12:18:50 -0700

    date validation and new loan and return book form fixed
    date validation and new loan and return book form fixed

--- models/loans.js
+++ models/loans.js
@@ -31,84 +31,115 @@ module.exports = (sequelize, DataTypes) => {
                                          }
                          },
                loaned_on: {
-                              allowNull: false,
+                               allowNull: false,
                                    type: DataTypes.DATEONLY,
                                validate: {
-                                            // validated as date
-                                          isDate: true,
+                                            // validated as today's date
                                           isTodayDate: function(value){
 
-                                            var Year = this.loaned_on.slice(0,4);
-                                            var Month = ( parseInt(this.loaned_on.slice(5,7)) - 1);
-                                            var Day = this.loaned_on.slice(8);
-                                            var submittedDate = new Date(Year, Month , Day);
+                                            if (value === 'Invalid date'){
+                                              throw new Error('Please enter a date, format: yyyy-mm-dd');
+                                            } else {
 
-                                            var now = new Date();
-                                            var testYear = now.getFullYear();
-                                            var testMonth = now.getMonth();
-                                            var testDate = now.getDate();
-                                            if (Year !== testYear || Month !== testMonth || Day !== testDate){
-                                              throw new Error(`Please enter todays date. format: yyyy-mm-dd`)
+                                                var Year = parseInt(value.slice(0,4));
+                                                var Month = ( parseInt(value.slice(5,7)) - 1);
+                                                var Day = parseInt(value.slice(8));
+
+                                                if (isNaN(Year) || isNaN(Month) || isNaN(Day)){
+
+                                                  throw new Error(`Please enter a date. format: yyyy-mm-dd`)
+                                                } else {
+
+                                                    var submittedDate = new Date(Year, Month , Day);
+
+                                                    var today = new Date();
+                                                    var oneDay = 24*60*60*1000;
+
+                                                    var diffDays = Math.round(Math.abs((submittedDate.getTime() - today.getTime())/(oneDay)));
+
+                                                    if (diffDays > 7 ){
+                                                      throw new Error('Please enter a date within the last week');
+                                                    }
+
+                                                }
+
                                             }
                                          }
+                                      }
                          },
                return_by: {
                               allowNull: false,
                                    type: DataTypes.DATEONLY,
                                validate: {
-                                            // validated as date
-                                          isDate: true,
-                                          },
-                                          plus7days: function(value){  // check if loan is for no more than 7 days
-                                              var lYear = this.loaned_on.slice(0,4);
-                                              var lMonth = ( parseInt(this.loaned_on.slice(5,7)) - 1);
-                                              var lDay = this.loaned_on.slice(8);
-                                              var rYear = value.slice(0,4);
-                                              var rMonth = ( parseInt(value.slice(5,7)) - 1 );
-                                              var rDay = value.slice(8);
+                                          // validate as date 7 days after loaned_on date
+                                              plus7days: function(value){  // check if loan is for no more than 7 days
 
-                                              var startLoan = new Date(lYear, rMonth , lDay);
-                                              var endLoan = new Date(rYear, rMonth, rDay);
+                                                  if (value === 'Invalid date'){
 
-                                              var oneDay = 24*60*60*1000;
+                                                      throw new Error('Please, enter a date, format: yyyy-mm-dd');
 
-                                              var diffDays = Math.round(Math.abs((startLoan.getTime() - endLoan.getTime())/(oneDay)));
+                                                  } else {
 
-                                              if (diifDays < 1 || diffDays > 7 ){
-                                                throw new Error('A book should be checked out no more than 7 days')
+                                                      var lYear = this.loaned_on.slice(0,4);
+                                                      var lMonth = ( parseInt(this.loaned_on.slice(5,7)) - 1);
+                                                      var lDay = this.loaned_on.slice(8);
+                                                      var rYear = value.slice(0,4);
+                                                      var rMonth = ( parseInt(value.slice(5,7)) - 1 );
+                                                      var rDay = value.slice(8);
+
+                                                      var startLoan = new Date(lYear, lMonth , lDay);
+                                                      var endLoan = new Date(rYear, rMonth, rDay);
+
+                                                      var oneDay = 24*60*60*1000;
+
+                                                      var diffDays = Math.round(Math.abs((startLoan.getTime() - endLoan.getTime())/(oneDay)));
+
+                                                      if (diffDays < 1 || diffDays > 7 ){
+
+                                                        throw new Error('Please, enter a date that is at least 7 days after the loaned on date')
+                                                      }
+
+                                                  }
+
                                               }
-
                                           }
-                                         }
                          },
              returned_on: {
                                    type: DataTypes.DATEONLY,
                                validate: {
-                                            // validated as date
-                                          isDate: true,
-                                          isTodayDate: function(value){
+                                            // validated as today's date
+                                            isTodayDate: function(value){
 
-                                            var Year = this.loaned_on.slice(0,4);
-                                            var Month = ( parseInt(this.loaned_on.slice(5,7)) - 1);
-                                            var Day = this.loaned_on.slice(8);
-                                            var submittedDate = new Date(Year, Month , Day);
+                                                if (value === 'Invalid date'){
 
-                                            var now = new Date();
-                                            var testYear = now.getFullYear();
-                                            var testMonth = now.getMonth();
-                                            var testDate = now.getDate();
-                                            if (Year !== testYear || Month !== testMonth || Day !== testDate){
-                                              throw new Error(`Please enter todays date. format: yyyy-mm-dd`)
+                                                  throw new Error('Please enter a date, format: yyyy-mm-dd');
+
+                                                } else {
+                                                  var Year = parseInt(value.slice(0,4));
+                                                  var Month = ( parseInt(value.slice(5,7)) - 1);
+                                                  var Day = parseInt(value.slice(8));
+                                                  var submittedDate = new Date(Year, Month , Day);
+
+                                                  var now = new Date();
+                                                  var testYear = now.getFullYear();
+                                                  var testMonth = now.getMonth();
+                                                  var testDate = now.getDate();
+                                                  if (Year !== testYear || Month !== testMonth || Day !== testDate){
+                                                    throw new Error(`Please enter todays date. format: yyyy-mm-dd`)
+                                                  }
+
+                                                }
+
                                             }
-                                         }
-                                          }
+
+                                        }
                       },
              current: {
                              allowNull: false,
                              type: DataTypes.BOOLEAN,
-
                       }
-      }, {
+      },
+      {
               timestamps: false,
              underscored: true
       }
